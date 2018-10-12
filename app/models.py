378896a87f5964a26e9aacbfec54c2eb04b22e48
lin <t-mac-1990@163.com>
2018-10-12 10:09:55 +0800

    优化

--- app/models.py
+++ app/models.py
@@ -2,58 +2,6 @@
 from . import db
 
 
-# class App(db.Model):
-#     __tablename__ = 'apps'
-#     id = db.Column(db.Integer, primary_key=True)
-#     trackId = db.Column(db.String(16), unique=True)
-#     trackCensoredName = db.Column(db.String(32))
-#     version = db.Column(db.String(16))
-#     releaseDate = db.Column(db.String(32))
-#     releaseNotes = db.Column(db.Text)
-#     description = db.Column(db.Text)
-#     screenShotUrls = db.Column(db.String(1024))
-#     genres = db.Column(db.String)
-#     artworkUrl60 = db.Column(db.String(256))
-#     artworkUrl100 = db.Column(db.String(256))
-#     artworkUrl512 = db.Column(db.String(256))
-#     trackViewUrl = db.Column(db.String(256))
-#     appType = db.Column(db.String(16))
-#     registerDate = db.Column(db.String(32))
-#     registerDays = db.Column(db.Integer)
-#     artistId = db.Column(db.String(16), db.ForeignKey('artists.artistId'))
-#     update_days = db.Column(db.Integer)
-#
-#     # def __repr__(self):
-#     #     return '<AppName %r>' % self.trackCensoredName
-#     def __repr__(self):
-#         return '<TrackId %s>' % self.trackId.encode('utf-8')
-#
-#     def int_to_str(self, id):
-#         return str(id).decode('utf-8')
-#
-#     def get_genres(self):
-#         app_genres = eval(self.genres)
-#         res = u''
-#         for genre in app_genres:
-#             res = res + ' ' + genre
-#         return res
-#
-#     # 获得app的更新信息(list形式)
-#     def get_app_release_notes(self):
-#         notes = eval(self.releaseNotes)
-#         return notes
-#
-#     def get_app_screen_shot_suffixes(self):
-#         app_screen_shot_number = eval(self.screenShotUrls)
-#         res = ['/screen_shot_0.jpg', '/screen_shot_1.jpg', '/screen_shot_2.jpg',
-#                '/screen_shot_3.jpg', '/screen_shot_4.jpg', '/screen_shot_5.jpg']
-#         number = len(app_screen_shot_number)
-#         if number > 4:
-#             return res[0:4]
-#         else:
-#             return res[0:number]
-
-
 class Artist(db.Model):
     __tablename__ = 'artists'
     id = db.Column(db.Integer, primary_key=True)
